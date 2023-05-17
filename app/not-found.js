fac9921942f9cd803e8ceae592ad9b5e2be39b67
akash <akashsarki12345@gmail.com>
2023-05-17 16:45:26 +0530

    not found page updated

--- app/not-found.js
+++ app/not-found.js
@@ -2,8 +2,8 @@ import React from 'react'
 
 const notfound = () => {
     return (
-        <div className='container2 text-center py-64'>
-            <h2 className='text-5xl font-bold mb-12'>Oops... Error 404</h2>
+        <div className='container2 text-center h-screen'>
+            <h2 className='text-5xl font-bold my-12'>Oops... Error 404</h2>
             <p className='mb-2'>The Page your are looking for is currently not available or lost.</p>
             <p>Or, go back to <a href='/'  className='btn'>HomePage</a></p>
         </div>

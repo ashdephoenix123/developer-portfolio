6609bad19b5db56ec52b568c61e61f5e8610eb37
akash <akashsarki12345@gmail.com>
2023-05-17 07:40:01 +0530

    responsiveness update

--- app/page.js
+++ app/page.js
@@ -11,14 +11,23 @@ export default function Home() {
           <div className="left basis-1/2 my-auto heroSection  max-[350px]:px-3">
             <h1 className="font-semibold text-4xl text-gray-700">Welcome You<span className="font-serif">,</span></h1>
             <p className="italic mb-4">Building a better web, one line of code at a time.</p>
-            <p className="pr-16 mb-4 removePad1  max-[394px]:text-sm">Want to bring your digital ideas to life through clean, modern, and functional websites and web applications? From conception to launch, I work with you every step of the way to ensure that your online presence accurately reflects your brand and meet your needs.</p>
+            <p className="pr-16 mb-4 removePad1  max-[394px]:text-sm"><span className="block">Want to bring your digital ideas to life through clean, modern, and functional websites and web applications?</span> From conception to launch, I work with you every step of the way to ensure that your online presence accurately reflects your brand and meet your needs.</p>
             <button className="btn max-[350px]:block max-[350px]:mb-2">Explore Blogs</button>
             <Link href={'/resume.pdf'} target="_blank" className="ml-2 btn  max-[350px]:ml-0">View Resumé</Link>
           </div>
           <div className="right relative basis-1/2 clipIt clipOver">
             <Image src='/3.jpg' fill className='object-cover' alt="hero-image"></Image>
           </div>
-          <Image src='/3.jpg' fill className='object-cover disp' alt="hero-image2"></Image>
+        </div>
+        <div className="w-full disp">
+          {/* <Image src='/3.jpg' fill className='w-full object-cover opacity-90' alt="hero-image2"></Image> */}
+          <div className="px-4 py-12 my-auto bg-gradient-to-r from-gray-400 to-gray-200">
+            <h1 className="font-semibold text-4xl text-gray-700">Welcome You<span className="font-serif">,</span></h1>
+            <p className="italic mb-4 text-sm -mt-0.5">Building a better web, one line of code at a time.</p>
+            <p className="mb-6 text-sm min-[520px]:pr-12"><span className="block">Want to bring your digital ideas to life through web applications? </span>From conception to launch, I work with you every step of the way to ensure that your online presence accurately reflects your brand and meet your needs.</p>
+            {/* <button className="btn">Explore Blogs</button> */}
+            <Link href={'/resume.pdf'} target="_blank" className="btn">View Resumé</Link>
+          </div>
         </div>
         <div className="aboutSection flex flex-wrap items-center max-[866px]:block min-[950px]:padd mt-12 min-[950px]:mt-28" id="about">
           <div className="left relative basis-1/3 overflow-hidden rounded-2xl shade1">
@@ -26,7 +35,8 @@ export default function Home() {
           </div>
           <div className="right basis-2/3 max-[866px]:pl-0 pl-16">
             <h2 className="head1 text-center">About Me<span className="font-serif">,</span></h2>
-            <div className="text-center text-sm italic">
+            <Image src='/me3.jpg' width={250} height={250} className='min-[866px]:hidden rounded-3xl mx-auto' alt="hero-image"></Image>
+            <div className="text-center text-sm italic max-[866px]:mt-4">
               <p className="mb-4">Hello and welcome! I am a passionate web developer with a love for creating beautiful, functional websites and web applications. My journey into web development began with a curiosity for how websites worked, and has since evolved into a career focused on bringing my clients' digital visions to life.</p>
               <p className="mb-4">With a strong foundation in front-end development using HTML, CSS, and JavaScript, I am able to create engaging user interfaces that are responsive across all devices. I am also comfortable working with back-end technologies such as Node.js, and databases such as MongoDB.</p>
               <p className="max-[530px]:hidden">When I'm not coding, you can find me exploring the outdoors and trying out new foods. I am always looking for ways to challenge myself and continue learning, and I am excited for the opportunity to work with you and bring your digital ideas to life."</p>

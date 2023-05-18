2414c310ac45286203bb7450aac02f3c97d88d6f
akash <akashsarki12345@gmail.com>
2023-05-18 19:24:32 +0530

    added a note for development

--- app/blogs/page.js
+++ app/blogs/page.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { AiOutlineSearch } from 'react-icons/ai'
 import BlogCard from '../component/BlogCard'
+import Link from 'next/link'
 
 const page = () => {
     return (
@@ -18,7 +19,11 @@ const page = () => {
                     </div>
                     <div className='my-4'>
                         <BlogCard />
-                        
+
+                    </div>
+                    <div className='w-full border-2 bg-gray-50 border-gray-100 text-black rounded-lg p-4 self-start max-[880px]:ml-0 max-[880px]:mt-4 max-[880px]:w-full dark:bg-gray-600 dark:border-gray-600 dark:text-zinc-300'>
+                        <h2 className='font-semibold head3 dark:text-neutral-300 mb-2'>Note</h2>
+                        <p className='mb-4 fontsm min-[1200px]:text-base'>This part of the website is still in production. What you are seeing is just a layout sample of the blog which is to be deployed in production in coming future. Inconvenience is deeply regretted and Thanks for visiting my website. You can contact me from the socials or the contact page from the <Link href={'/'} className='underline'>Home</Link> page.</p>
                     </div>
                 </div>
             </div>

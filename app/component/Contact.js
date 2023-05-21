315bc7339cd51ac9186aa2140c4ad3b644e0960b
akash <akashsarki12345@gmail.com>
2023-05-21 19:02:49 +0530

    added sendgrid api

--- app/component/Contact.js
+++ app/component/Contact.js
@@ -1,7 +1,7 @@
 'use client';
 
 import React, { useState } from 'react'
-// import { toast } from 'react-toastify';
+import { toast } from 'react-toastify';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
@@ -23,45 +23,45 @@ const Contact = () => {
         })
     }
 
-    // const handleSubmit = async (e) => {
-    //     e.preventDefault();
-    //     const dataSend = await fetch('/api/contact', {
-    //         method: 'POST',
-    //         headers: {
-    //             'Content-Type': 'application/json'
-    //         },
-    //         body: JSON.stringify(details)
-    //     })
-    //     const data = await dataSend.json();
-    //     if (data.message === 'success') {
-    //         toast.success('Message sent!', {
-    //             position: "top-right",
-    //             autoClose: 2000,
-    //             hideProgressBar: false,
-    //             closeOnClick: true,
-    //             pauseOnHover: true,
-    //             draggable: true,
-    //             progress: undefined,
-    //             theme: "light",
-    //         });
-    //         setDetails({
-    //             email: "",
-    //             subject: "",
-    //             message: ""
-    //         })
-    //     } else {
-    //         toast.error('Some error occured, Please try again!', {
-    //             position: "top-right",
-    //             autoClose: 2000,
-    //             hideProgressBar: false,
-    //             closeOnClick: true,
-    //             pauseOnHover: true,
-    //             draggable: true,
-    //             progress: undefined,
-    //             theme: "light",
-    //         });
-    //     }
-    // }
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        const dataSend = await fetch('/api/contact', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(details)
+        })
+        const data = await dataSend.json();
+        if (data.message === 'success') {
+            toast.success('Message sent!', {
+                position: "top-right",
+                autoClose: 2000,
+                hideProgressBar: false,
+                closeOnClick: true,
+                pauseOnHover: true,
+                draggable: true,
+                progress: undefined,
+                theme: "light",
+            });
+            setDetails({
+                email: "",
+                subject: "",
+                message: ""
+            })
+        } else {
+            toast.error('Some error occured, Please try again!', {
+                position: "top-right",
+                autoClose: 2000,
+                hideProgressBar: false,
+                closeOnClick: true,
+                pauseOnHover: true,
+                draggable: true,
+                progress: undefined,
+                theme: "light",
+            });
+        }
+    }
 
     return (
         <div className="contactbg flex flex-col justify-center w-full py-20 mt-12 min-[950px]:mt-28">
@@ -79,7 +79,7 @@ const Contact = () => {
             />
             <div className="contact min-[950px]:padd mb-12 text-center" id="contact">
                 <h2 className="head2">Contact Me<span className="font-serif">,</span></h2>
-                <form action="https://formspree.io/f/xyyajalr" method="post" className="flex flex-col mx-auto mt-22 text-black fontsm w-[90%] min-[710px]:w-[70%] min-[950px]:w-1/3">
+                <form onSubmit={handleSubmit} className="flex flex-col mx-auto mt-22 text-black fontsm w-[90%] min-[710px]:w-[70%] min-[950px]:w-1/3">
                     <input required onChange={handleDetails} name='email' value={details.email} type="email" placeholder="name@email.com" className="dark:text-white rounded my-1 px-3 py-1.5 outline-none" />
                     <input required onChange={handleDetails} name='subject' value={details.subject} type="text" placeholder="Subject" className="dark:text-white rounded my-1 px-3 py-1.5 outline-none" />
                     <textarea required onChange={handleDetails} name='message' value={details.message} cols="30" rows="10" placeholder="Leave a comment..." className="dark:text-white rounded my-1 px-3 py-2 outline-none"></textarea>

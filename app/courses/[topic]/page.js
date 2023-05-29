72542dd813f6702be37f3369e975352db13e9b56
akash <akashsarki12345@gmail.com>
2023-05-29 10:06:20 +0530

    added

---
+++ app/courses/[topic]/page.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const page = ({params}) => {
+    const {topic} = params;
+  return (
+    <div className='container2'>
+      topic
+    </div>
+  )
+}
+
+export default page
--- app/courses/page.js
+++ app/courses/page.js
@@ -7,7 +7,12 @@ const page = () => {
         <div className='container2'>
             <div className='my-12'>
             <h2 className='font-semibold text-2xl text2 max-[716px]:text-center'>All Courses</h2>
-               <Card2 />
+               <Card2
+                title='Learn Javascript'
+                img='/jsimg2.png'
+                description='JavaScript is a language of the web, Master the fundamentals of JavaScript and develop dynamic web applications with our comprehensive course.'
+                topic='javascript'
+                />
             </div>
         </div>
     </>

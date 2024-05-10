7fb74a83d1dcf8cc1e9cf60597942ca639140e62
Akash Sarki <102145279+ashdephoenix123@users.noreply.github.com>
2024-05-10 16:45:08 +0530

    updated the all projects link
    line 148

--- app/page.js
+++ app/page.js
@@ -145,7 +145,7 @@ export default function Home() {
               <div className="slide relative">
                 <Image src={'/proj6.png'} fill className="imageInSlide" alt="projectimg"></Image>
                 <div className="projBack">
-                  <Link href={'https://www.thesharkkcompany.tech/'} target="_blank" className="btn-outline absolute top-1/2 left-1/2 -translate-x-2/4 -translate-y-2/4 flex items-center">VISIT <BsArrowUpRight className="ml-1" /></Link>
+                  <Link href={'https://akashsarki-projects.vercel.app/'} target="_blank" className="btn-outline absolute top-1/2 left-1/2 -translate-x-2/4 -translate-y-2/4 flex items-center">VISIT <BsArrowUpRight className="ml-1" /></Link>
                 </div>
 
               </div>

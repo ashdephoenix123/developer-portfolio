91399157f48240ed4291b192994efc0f96befb1c
ashdephoenix123 <akashsarki12345@gmail.com>
2025-10-09 20:53:40 +0530

    canged projects layout

--- app/component/Projects.js
+++ app/component/Projects.js
@@ -1,26 +1,26 @@
 "use client";
 
+import { motion } from "framer-motion";
 import "swiper/css";
 import "swiper/css/navigation";
-import { Navigation } from "swiper/modules";
-import { Swiper, SwiperSlide } from "swiper/react";
 import ProjectCard from "../component/ProjectCard";
-import { motion } from "framer-motion";
 
 const containerVariant = {
   initial: { opacity: 1 },
   final: {
     opacity: 1,
     transition: {
+      delayChildren: 1.1,
       staggerChildren: 0.25,
     },
   },
 };
 
 const cardVariant = {
-  initial: { x: 350 },
+  initial: { y: 50, opacity: 0 },
   final: {
-    x: 0,
+    y: 0,
+    opacity: 1,
     transition: { type: "spring", stiffness: 20 },
   },
 };
@@ -38,33 +38,15 @@ const Projects = ({ projects }) => {
         </svg>
         <h1 className="text-white">All Projects</h1>
       </motion.div>
-      <motion.div variants={containerVariant} initial="initial" animate="final">
-        <Swiper
-          speed={1500}
-          navigation={true}
-          modules={[Navigation]}
-          className="px-4 lg:px-0"
-          breakpoints={{
-            0: {
-              slidesPerView: 1,
-              spaceBetween: 20,
-            },
-            768: {
-              slidesPerView: 2,
-              spaceBetween: 30,
-            },
-            1024: {
-              slidesPerView: 3,
-              spaceBetween: 30,
-            },
-          }}
-        >
-          {projects.map((project) => (
-            <SwiperSlide key={project.id}>
-              <ProjectCard project={project} animationVariant={cardVariant} />
-            </SwiperSlide>
-          ))}
-        </Swiper>
+      <motion.div
+        variants={containerVariant}
+        initial="initial"
+        animate="final"
+        className="grid md:grid-cols-2 lg:grid-cols-3 gap-6 lg:gap-8"
+      >
+        {projects.map((project) => (
+          <ProjectCard project={project} animationVariant={cardVariant} />
+        ))}
       </motion.div>
     </div>
   );
